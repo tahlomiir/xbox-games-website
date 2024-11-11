d2c8e98cd0705e19ea1b0941c1415ab11a6b325e
Orion Starfallen <Starfallen-O@ulster.ac.uk>
2024-11-11 21:32:27 +0000

    Added requirements.txt file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def index():
     return render_template('/html/index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=4999)
+    port = int(os.environ.get("PORT", 4999))
+    app.run(debug=True, host='0.0.0.0', port=port)
